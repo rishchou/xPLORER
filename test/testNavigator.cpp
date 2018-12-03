4f0dbf865d222595914109385647f5ff13afb05e
akashatharv <akashatharv@gmail.com>
2018-12-03 12:54:04 -0500

    [7.2 : TST] Tests for navigator class are created and added to the test directory

---
+++ test/testNavigator.cpp
@@ -0,0 +1,70 @@
+/**============================================================================
+ * @file       : testNavigator.cpp
+ * @author     : Akash Atharv, Rishabh Choudhary
+ * @version    : 1.0
+ * @Copyright  : 3-Clause BSD
+Copyright (c) 2018, Akash Atharv, Rishabh Choudhary
+ 
+Redistribution and use in source and binary forms, with or without  
+modification, are permitted provided that the following conditions are 
+met:
+ 
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+ 
+2. Redistributions in binary form must reproduce the above copyright 
+notice, this list of conditions and the following disclaimer in the   
+documentation and/or other materials provided with the distribution.
+ 
+3. Neither the name of the copyright holder nor the names of its 
+contributors may be used to endorse or promote products derived from this 
+software without specific prior written permission.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+ * @brief      : Test file for testing class Navigator
+ *============================================================================
+ */
+
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+#include "../include/xplorer/obstacleDetector.hpp"
+#include "../include/xplorer/navigator.hpp"
+
+class TestClass {
+ private:
+         geometry_msgs::Twist twist;
+ public:
+         void publish( const geometry_msgs::Twist::ConstPtr& msg) {
+         twist = *msg;
+}
+};               
+
+TEST(TESTSuite, Navigator_Object_is_initialized) {
+        EXPECT_NO_FATAL_FAILURE(navigator navigator);
+}
+
+TEST(TESTSuite, Subscribe_Test) {
+        ros::NodeHandle n6;
+        auto pub = n6.advertise<std_msgs::Float64>("/dist",1000);
+        ros::Duration(1).sleep();
+        EXPECT_EQ(pub.getNumSubscribers(),1);
+}
+
+TEST(TESTSuite, Publish_Test) {
+        ros::NodeHandle n7;
+        TestClass test;
+        navigator navigator;
+        auto sub = n7.subscribe("/cmd_vel_mux/input/navi", 1000,&TestClass::publish,&test);
+        EXPECT_EQ(1, sub.getNumPublishers());
+}
