d8755991cec30931fe4d8a0b11a865ef99df3c34
akashatharv <akashatharv@gmail.com>
2018-12-03 12:53:04 -0500

    [7.1 : TST] Tests for the obstacleDetector class are created and added to the test directory

---
+++ test/testObstacleDetector.cpp
@@ -0,0 +1,75 @@
+/**============================================================================
+ * @file       : testObstacleDetector.cpp
+ * @author     : Akash Atharv, Rishabh Choudhary
+ * @version    : 1.0
+ * @Copyright  : 3-Clause BSD
+Copyright (c) 2018, Akash Atharv, Rishabh Choudhary
+ 
+Redistribution and use in source and binary forms, with or without  
+modification, are permitted provided that the following conditions are 
+met:
+ 
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+ 
+2. Redistributions in binary form must reproduce the above copyright 
+notice, this list of conditions and the following disclaimer in the   
+documentation and/or other materials provided with the distribution.
+ 
+3. Neither the name of the copyright holder nor the names of its 
+contributors may be used to endorse or promote products derived from this 
+software without specific prior written permission.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+ * @brief      : Test file for testing class obstacleDetector
+ *============================================================================
+ */
+
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+#include "../include/xplorer/obstacleDetector.hpp"
+#include "../include/xplorer/navigator.hpp"
+
+class testClass {
+ private:
+ public:
+         void publishVal(const sensor_msgs::LaserScan::ConstPtr& msg) {
+                 float val = 1000;
+                 for (const auto& i : msg->ranges) {
+                         if (i < val) {
+                              val = i;
+                         }
+                 }
+}
+};
+
+TEST(TESTSuite, ObstacleDetector_Object_is_initialized) {
+        EXPECT_NO_FATAL_FAILURE(obstacleDetector obs);
+}
+
+TEST(TESTSuite, Collision_should_not_be_detected) {
+        obstacleDetector obs;
+        ros::NodeHandle n4;
+        ros::Duration(25).sleep();
+        ros::spinOnce();
+        EXPECT_TRUE(~obs.collisionDetect());
+}
+
+TEST(TESTSuite, Collision_should_be_detected) {
+        obstacleDetector obs;
+        ros::NodeHandle n5;
+        ros::Duration(6).sleep();
+        ros::spinOnce();
+        EXPECT_TRUE(obs.collisionDetect());
+}
